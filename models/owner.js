6ba4154b4597c937d90c252118b8f2b680727751
Rutik Ghaskata <rutik.g@codesquote.com>
2022-02-07 15:06:35 +0530

    new

--- models/owner.js
+++ models/owner.js
@@ -32,42 +32,42 @@ const ownerSchema = new mongoose.Schema({
     }
 })
 
-ownerSchema.pre('save',function(next){
-    const owner = this;
-    if(!owner.isModified('password','confirmPassword')){
-        return next()
-    }
-    bcrypt.genSalt(10,(err,salt)=>{
-        if(err){
-            return next(err)
-        }
-     bcrypt.hash(owner.password,salt,(err,hash)=>{
-         if(err){
-             return next(err)
-         }
-         owner.password = hash;
-         owner.confirmPassword = hash;
-         next()
-     })
-    })
+// ownerSchema.pre('save',function(next){
+//     const owner = this;
+//     if(!owner.isModified('password','confirmPassword')){
+//         return next()
+//     }
+//     bcrypt.genSalt(10,(err,salt)=>{
+//         if(err){
+//             return next(err)
+//         }
+//      bcrypt.hash(owner.password,salt,(err,hash)=>{
+//          if(err){
+//              return next(err)
+//          }
+//          owner.password = hash;
+//          owner.confirmPassword = hash;
+//          next()
+//      })
+//     })
 
-})
+// })
 
 
-ownerSchema.methods.comparePassword = function(candidatePassword) {
-    const owner = this;
-    return new Promise((resolve,reject)=>{
-        bcrypt.compare(candidatePassword,owner.password,(err,isMatch)=>{
-            if(err){
-                return reject(err)
-            }
-            if (!isMatch){
-                return reject(err)
-            }
-            resolve(true)
-        })
-    })
+// ownerSchema.methods.comparePassword = function(candidatePassword) {
+//     const owner = this;
+//     return new Promise((resolve,reject)=>{
+//         bcrypt.compare(candidatePassword,owner.password,(err,isMatch)=>{
+//             if(err){
+//                 return reject(err)
+//             }
+//             if (!isMatch){
+//                 return reject(err)
+//             }
+//             resolve(true)
+//         })
+//     })
 
-}
+// }
 
  mongoose.model('owner',ownerSchema);
